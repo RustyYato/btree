dad8c27fe229ef405d73acd04923c6453d39b2af
RustyYato <rustyyato@gmail.com>
2026-06-10 18:03:37 -0500

    initial port from std

---
+++ btree/src/lib.rs
@@ -0,0 +1,29 @@
+#![feature(
+    allocator_api,
+    trusted_len,
+    hasher_prefixfree_extras,
+    extend_one,
+    dropck_eyepatch,
+    core_intrinsics,
+    slice_ptr_get,
+    min_specialization
+)]
+
+extern crate alloc;
+
+mod append;
+mod borrow;
+mod dedup_sorted_iter;
+mod fix;
+mod map;
+mod mem;
+mod merge_iter;
+mod navigate;
+mod node;
+mod remove;
+mod search;
+mod set;
+mod set_val;
+mod split;
+
+mod testing;
